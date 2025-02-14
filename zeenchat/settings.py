49b49431f8589c5d9b8975f06e147562525d2403
Baha <baha.dev@outlook.com>
2025-02-14 11:28:33 +0300

    add UserStatusMiddleware to track user online status

--- zeenchat/settings.py
+++ zeenchat/settings.py
@@ -64,6 +64,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'chatapp.middleware.UserStatusMiddleware',
 ]
 
 ROOT_URLCONF = 'zeenchat.urls'
